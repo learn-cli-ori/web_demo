77deb71e1a6ac247bda52eccfed267859baf61ac
liyuhan <719614719@qq.com>
2020-01-07 11:30:40 +0800

    模拟获取权限接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,19 @@
 import { request } from '@/utils/request';
 
 // 获取权限
-export const getAdminAuthority = params => request().get('/api/users/getAdminAuthority', params);
+// export const getAdminAuthority = params => request().get('/api/users/getAdminAuthority', params);
+export const getAdminAuthority = params => {
+    //模拟接口
+    return {
+        'code': 200,
+        "data": [{
+            path: '/test/index',
+            name: 'test',
+            filePath: "/test/test"
+        }],
+        "msg": "操作成功"
+    };
+}
 
 // 登录
 export const login = params => request().get('/api/users/login', params);
